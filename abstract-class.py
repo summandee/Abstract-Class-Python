1eb644bdaaa215909b5bddcd5d1e93d07d5b1a3f
Naheed <89731453+summandee@users.noreply.github.com>
2024-11-14 04:38:18 -0800

    Update abstract-class.py

--- abstract-class.py
+++ abstract-class.py
@@ -1,5 +1,7 @@
-# prevents a user from creating an object of that class
-#  Compels a user to overrides abstaract methods in child class 
+# prevents a user from creating an object of that class.
+#  Compels a user to overrides abstaract methods in child class. 
+# abstaract  class = A class which  contains one or more abstract methods.
+# abstract method = A Method that has a declaration but does not have implementation.
 
 from abc import ABC,  abstractmethod
 class Vehicle:
